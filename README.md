3b030636db84d8ca13559a56fb85508844db1c3e
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2023-11-27 15:38:54 +0100

    first proper version

--- README.md
+++ README.md
@@ -1,12 +1,45 @@
-# my-first-go-component
-A Viam custom component Go example
+# My-First-Go-Component
+
+This is a basic example / starter kit to learn about how to build and integrate custom Viam components, using the Viam sensor API. This setup can easily be extended for other component API types listed in the Viam documentation here: [Viam Component APIs](https://docs.viam.com/build/program/apis/#component-apis).
 
 
+## Useful Go Setup and Build Commands
 
-## Setup a new Go project
+Some commonly used commands. Maybe helpful maybe not :-)
 
 ```go mod init mysensor```
 
 ```go mod tidy```
 
 ```go build -o ./build/mysensor ./src```
+
+## Add a Local Module
+
+Configure a local module through the web interface or add the following to the RAW JSON.
+
+```
+{
+  "name": "a-module-name",
+  "executable_path": "<-- Path to the sensor binary including the binary -->",
+  "type": "local"
+}
+```
+
+
+
+## Configure Component
+
+Add this configuration to the smart machine "components" part either in RAW JSON mode or through the UI and "local component".
+
+```
+{
+  "name": "gosensor",
+  "model": "viam-soleng:sensor:mysensor",
+  "type": "sensor",
+  "namespace": "rdk",
+  "attributes": {
+    "setting": 3
+  },
+  "depends_on": []
+}
+```
--- src/mysensor.go
+++ src/mysensor.go
@@ -1,10 +1,11 @@
-// Package mybase implements a base that only supports SetPower (basic forward/back/turn controls), IsMoving (check if in motion), and Stop (stop all motion).
-// It extends the built-in resource subtype Base and implements methods to handle resource construction, attribute configuration, and reconfiguration.
+// This module implements a simple sensor returning the value of the setting attribute in the sensor's configuration.
+// It extends the built-in resource subtype Sensor and implements its methods to handle resource construction, attribute configuration, and reconfiguration.
 
 package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/pkg/errors"
 
@@ -15,80 +16,86 @@ import (
 	"go.viam.com/utils"
 )
 
-// Here is where we define your new model's colon-delimited-triplet (acme:demo:mybase)
-// acme = namespace, demo = repo-name, mybase = model name.
-var (
-	Model            = resource.NewModel("viam-soleng", "sensor", "mysensor")
-	errUnimplemented = errors.New("unimplemented")
-)
-
-type mySensor struct {
-	resource.Named
-	logger logging.Logger
-}
-
+// Module initializer which registers this component with the Viam RDK register.
 func init() {
 	resource.RegisterComponent(sensor.API, Model, resource.Registration[sensor.Sensor, *Config]{
 		Constructor: newSensor,
 	})
 }
 
+// Here you define your new model's colon-delimited-triplet (acme:demo:mybase). If you plan to upload this module to the Viam registry, "acme" must match your registry namespace.
+// acme = namespace, demo = repo-name, mybase = model name.
+var (
+	Model            = resource.NewModel("viam-soleng", "sensor", "mysensor")
+	errUnimplemented = errors.New("unimplemented")
+)
+
+// The sensor struct with the value attribute which will be returned by the sensor "readings" method.
+type mySensor struct {
+	resource.Named
+	logger logging.Logger
+	value  int
+}
+
+// Sensor constructor
 func newSensor(ctx context.Context, deps resource.Dependencies, conf resource.Config, logger logging.Logger) (sensor.Sensor, error) {
-	b := &mySensor{
+	s := &mySensor{
 		Named:  conf.ResourceName().AsNamed(),
 		logger: logger,
 	}
-	if err := b.Reconfigure(ctx, deps, conf); err != nil {
+	if err := s.Reconfigure(ctx, deps, conf); err != nil {
 		return nil, err
 	}
-	return b, nil
+	return s, nil
 }
 
-// Config contains two component (motor) names.
+// Maps JSON component configuration attributes.
 type Config struct {
-	//LeftMotor  string `json:"motorL"`
-	//RightMotor string `json:"motorR"`
+	Setting int `json:"setting"`
 }
 
-// Validate validates the config and returns implicit dependencies,
+// Implement component configuration validation and and return implicit dependencies.
 func (cfg *Config) Validate(path string) ([]string, error) {
+
+	if cfg.Setting == 0 {
+		return nil, fmt.Errorf(`expected "setting" attribute bigger than 0 for mysensor %q`, path)
+	}
 	return []string{}, nil
 }
 
 // Reconfigure reconfigures with new settings.
-func (b *mySensor) Reconfigure(ctx context.Context, deps resource.Dependencies, conf resource.Config) error {
+func (s *mySensor) Reconfigure(ctx context.Context, deps resource.Dependencies, conf resource.Config) error {
 
 	// This takes the generic resource.Config passed down from the parent and converts it to the
 	// model-specific (aka "native") Config structure defined, above making it easier to directly access attributes.
-
-	/*
-		sensorConfig, err := resource.NativeConfig[*Config](conf)
-		if err != nil {
-			return err
-		}
-	*/
-	return nil
+	sensorConfig, err := resource.NativeConfig[*Config](conf)
+	if err != nil {
+		return err
+	}
+	s.logger.Infof(`Reconfiguring sensor value from %v to %v`, s.value, sensorConfig.Setting)
+	s.value = sensorConfig.Setting
+	return err
 }
 
 // Get sensor reading
 func (s *mySensor) Readings(ctx context.Context, _ map[string]interface{}) (map[string]interface{}, error) {
-	return map[string]interface{}{"hello": "world"}, nil
+	return map[string]interface{}{"setting": s.value}, nil
 }
 
-// DoCommand simply echos whatever was sent.
-func (b *mySensor) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
+// DoCommand can be implemented to extend sensor functionality but returns unimplemented in this example.
+func (s *mySensor) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
 	return nil, errUnimplemented
 }
 
-// Close function which is exectuted when the component is shut down
-func (b *mySensor) Close(ctx context.Context) error {
+// The close method is executed when the component is shut down
+func (s *mySensor) Close(ctx context.Context) error {
 	return nil
 }
 
 func main() {
 	// NewLoggerFromArgs will create a logging.Logger at "DebugLevel" if
 	// "--log-level=debug" is an argument in os.Args and at "InfoLevel" otherwise.
-	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("yourmodule"))
+	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("My Go Sensor Module"))
 }
 
 func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) (err error) {
